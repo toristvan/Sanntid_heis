0838c806aac47c458daa8ad7bc9ea2768deefce0
Paal Thorseth <pathorse@stud.ntnu.no>
2019-03-21 16:24:57 +0100

    Add queuepolling, remove elevinit

--- elevatorClient.go
+++ elevatorClient.go
@@ -2,162 +2,182 @@ package main
 
 import (
     "./driverModule/elevio"
-    ."fmt"
-		"time"
+    "./queueModule"
+    .	"fmt"
+	"time"
 )
 
-type state int
+type ElevStateType int
 const (
-	idle 			state = 0
-	goingUp 	state = 1
-	goingDown state = 2
-	atFloor 	state = 4
+	idle 				ElevStateType = 0
+	goingUp 			ElevStateType = 1
+	goingDown 			ElevStateType = 2
+	atFloor 			ElevStateType = 4
 )
-type event int
+type elevCommand int
 const (
-	newOrder 			event = 0
-	goUp		 			event = 1
-	goDown 	 			event = 2
-	floorReached 	event = 3
-	finished 			event = 4
-  wait          event = 5
+	newOrder 			elevCommand = 0
+	goUp		 		elevCommand = 1
+	goDown 	 			elevCommand = 2
+	floorReached 		elevCommand = 3
+	finished 			elevCommand = 4
+  	wait          		elevCommand = 5
 )
 
-var State state
-var new_Event event
+const localID = 0
 
-func initElevToKnownFloor() int {
-  var floor int
-  var done bool
-  drv_floors  := make(chan int)
+var elev_state ElevStateType
+var new_command elevCommand
+/*
+func initElev() { //Change name?
+  	var floor int
+  	var done bool
 
-  go elevio.PollFloorSensor(drv_floors)
-
-  for{
-    select{
-    case sensor := <- drv_floors:
-        floor = sensor
-        done = true
-    default:
-      elevio.SetMotorDirection(elevio.MD_Down)
-    }
-    if done {
+  	
+  	for{
+	    select{
+	    case sensor := <- pull_floor:
+	      	floor = sensor
+	    	done = true
+	    default:
+	      	elevio.SetMotorDirection(elevio.MD_Down)
+	    }
+	    if done {
+	    	//Unnecessary?
 			elevio.SetMotorDirection(elevio.MD_Stop)
-      break
-    }
-  }
-  return floor
+    		break
+  		}
+  	}
 }
-
+*/
 func main(){
-    var buttonEvent elevio.ButtonEvent
-    floor_Order := buttonEvent.Floor
-    button := buttonEvent.Button
+    var current_order queue.OrderStruct
+    var current_floor int
+    //next_floor := current_order.Floor
+    //button := current_order.Button
 
     //wait_for_input := make(chan bool)
-    drv_buttons := make(chan elevio.ButtonEvent)
+
+    elevio.Init("localhost:15657")
+
     drv_floors  := make(chan int)
     drv_obstr   := make(chan bool)
     drv_stop    := make(chan bool)
+    order_chan := make(chan queue.OrderStruct)
 
-		elevio.Init("localhost:15657")
-		currentFloor := initElevToKnownFloor()
-		State = idle
-		Println("Ready")
+    defer close(drv_floors)
+    defer close(drv_obstr)
+    defer close(drv_stop)
+    defer close(order_chan)
 
-		go elevio.PollButtons(drv_buttons)
-		go elevio.PollFloorSensor(drv_floors)
-		go elevio.PollObstructionSwitch(drv_obstr)
-		go elevio.PollStopButton(drv_stop)
-    //go interruptButtons(drv_buttons, wait_for_input)
+    go elevio.PollFloorSensor(drv_floors)
+    go elevio.PollObstructionSwitch(drv_obstr)
+	go elevio.PollStopButton(drv_stop) 
+	go queue.Queue(order_chan)
+
+	elev_state = idle
+	current_floor = elevio.Num_floors + 1
+
+	Println("Ready")
+	Println("Current floor:", current_floor)
+
+	//go interruptButtons(drv_buttons, wait_for_input)
 
     for {
         select {
-        case button_Input := <- drv_buttons:
-            floor_Order = button_Input.Floor
-            button = button_Input.Button
-						elevio.SetButtonLamp(button, floor_Order, true)
+        case new_order := <- order_chan:   //Input from queue
+            next_floor := new_order.Floor
+            order_type := new_order.Button
+            current_order = new_order
 
-						switch button {
-						case 0, 1:
-							Println("Hall call")
-							Println("botton input:", button, "floor:", floor_Order)
+            //Add to watchdog here
 
-							if floor_Order < currentFloor{
-								new_Event = goDown
-							} else if floor_Order > currentFloor {
-								new_Event = goUp
-							} else {
-								new_Event = floorReached
-							}
+			elevio.SetButtonLamp(order_type, next_floor, true)
 
-						case 2:
-              //Til nå gjør denne casen det samme som hall call
-							Println("Cab call")
-							Println("botton input:", button, "floor:", floor_Order)
+			switch order_type {
+			case elevio.BT_HallUp, elevio.BT_HallDown:
+				Println("Hall call")
+				Println("Floor:", current_order.Floor)
 
-							if floor_Order < currentFloor{
-								new_Event = goDown
-							} else if floor_Order > currentFloor {
-								new_Event = goUp
-							} else {
-								new_Event = floorReached
-							}
-						}
+				if next_floor < current_floor{
+					new_command = goDown
+				} else if next_floor > current_floor {
+					new_command = goUp
+				} else {
+					new_command = floorReached
+				}
 
-        case floor_sensorInput := <- drv_floors:
-            currentFloor = floor_sensorInput
-            Println("currentFloor:", currentFloor)
+			case elevio.BT_Cab:
+  				//Til nå gjør denne casen det samme som hall call
+				Println("Cab call")
+				Println("Floor:", current_order.Floor)
 
-						if currentFloor == floor_Order {
-							elevio.SetButtonLamp(button, floor_Order, false)
-							new_Event = floorReached
-						}
+				if next_floor < current_floor{
+					new_command = goDown
+				} else if next_floor > current_floor {
+					new_command = goUp
+				} else {
+					new_command = floorReached
+				}
+			}
 
-				default:
+        case floor_input := <- drv_floors:
+            current_floor = floor_input
+            elevio.SetFloorIndicator(current_floor)
+            Println("Current floor:", current_floor)
 
-					switch State{
-					case idle:
-						elevio.SetDoorOpenLamp(false)
+			if current_floor == current_order.Floor {
+				elevio.SetButtonLamp(current_order.Button, current_floor, false)
+				queue.RemoveOrder(current_floor, localID)
+				new_command = floorReached
+			}
 
-						switch new_Event{
-						case goDown:
-							State = goingDown
-						case goUp:
-							State = goingUp
-						case floorReached:
-							State = atFloor
-						}
+		default:
+			switch elev_state{
+			case idle:
+				elevio.SetDoorOpenLamp(false)
 
-					case goingUp:
-						if new_Event == floorReached{
-							elevio.SetMotorDirection(elevio.MD_Stop)
-							State = atFloor
-						} else{
-							elevio.SetMotorDirection(elevio.MD_Up)
-              time.Sleep(100 * time.Millisecond)
-						}
+				switch new_command{
+				case goDown:
+					elev_state = goingDown
+				case goUp:
+					elev_state = goingUp
+				case floorReached:
+					elev_state = atFloor
+				}
 
-					case goingDown:
-						if new_Event == floorReached{
-							elevio.SetMotorDirection(elevio.MD_Stop)
-							State = atFloor
-						} else {
-							elevio.SetMotorDirection(elevio.MD_Down)
-              time.Sleep(100 * time.Millisecond)
-						}
+			case goingUp:
+				if new_command == floorReached{
+					elevio.SetMotorDirection(elevio.MD_Stop)
+					elev_state = atFloor
+				} else{
+					elevio.SetMotorDirection(elevio.MD_Up)
+      				time.Sleep(100 * time.Millisecond)
+				}
 
-					case atFloor:
-						elevio.SetDoorOpenLamp(true)
-						elevio.SetButtonLamp(button, floor_Order, false)
+			case goingDown:
+				if new_command == floorReached{
+					elevio.SetMotorDirection(elevio.MD_Stop)
+					elev_state = atFloor
+				} else {
+					elevio.SetMotorDirection(elevio.MD_Down)
+      				time.Sleep(100 * time.Millisecond)
+				}
 
-            Println("Floor reached")
-						time.Sleep(3000 * time.Millisecond)
+			case atFloor:
+				elevio.SetDoorOpenLamp(true)
+				for i := 0; i < 3; i++ {
+					elevio.SetButtonLamp(elevio.ButtonType(i), current_floor, false)
+				}
+				
 
-            new_Event = wait
-						State = idle
-					}
+    			Println("Floor reached")
+				time.Sleep(3000 * time.Millisecond)
 
-      }
+    			new_command = wait
+				elev_state = idle
+			}
+
+      	}
     }
 }
