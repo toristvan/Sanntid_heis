710be6fd865edea42862c11202d6870c6e473490
Paal Thorseth <pathorse@stud.ntnu.no>
2019-03-21 16:27:00 +0100

    Add Queue-handler goroutine. Unfinished add and remove

--- queueModule/queue.go
+++ queueModule/queue.go
@@ -8,29 +8,32 @@ import (
 
 const num_elevs int = 1
 const queue_size int = (elevio.Num_floors*3)-2
+const localID int = 0
+
 // type BT_Cab signalizises cab call
 // maybe add floorstop array
 type OrderStruct struct
 {
-	btn elevio.ButtonType
-	floor int
+	Button elevio.ButtonType
+	Floor int
 	timestamp time.Time
 }
 
 // flytte til main?
 var orderQueue [num_elevs][queue_size] OrderStruct
+
+
 func InitQueue(){
 	var invalidOrder OrderStruct
-	invalidOrder.btn = 0
-	invalidOrder.floor = -1
+	invalidOrder.Button = 0
+	invalidOrder.Floor = -1
 	for j := 0; j< num_elevs; j++{
 		for i := 0; i< queue_size; i++{
 			orderQueue[j][i] = invalidOrder
 		}
 	}
-	orderQueue[0][0].btn = 1
-	orderQueue[0][0].floor = 0
-	orderQueue[0][0].timestamp = time.Now()
+	orderQueue[localID][0].Floor = 0
+	orderQueue[localID][0].Button = elevio.BT_Cab
 }
 //var orderQueue := make([] ,queue_size )
 
@@ -48,44 +51,48 @@ func costFunction(newFloor int, currentFloor int, dir elevio.MotorDirection ) in
 	return cost
 }
 
-func AddOrder(order queue.OrderStruct, id int) {
+func addToQueue(order OrderStruct, id int) {
 	fmt.Printf("%+v\n", order)
 	
-	}
 	for i := 0; i< queue_size; i++{
-		if orderQueue[id][i].floor == -1 {
+		if orderQueue[id][i].Floor == -1 {
 			orderQueue[id][i] = order
 			break
 		}
 	}
-	elevio.SetButtonLamp(new_order.Button, new_order.Floor, true)	//Set lamp after order is sent to watchdog
+	fmt.Printf("Order added. Current queue: %+v\n", orderQueue)
 }
 
+/*
 func CreateOrder(floor int, btn elevio.ButtonType) OrderStruct{
 	var order OrderStruct
-	order.btn = btn
-	order.floor = floor
+	order.Button = btn
+	order.Floor = floor
 
 	fmt.Printf("Order added: %+v\n Order queue: %+v\n", order, orderQueue)
 	return order
 }
+*/
 
 func RemoveOrder(floor int, id int){
 	//Sletter alle ordre med oppgitt etasje i.
 	//Kan evt bare slette dem med gitt retning, men er det vits?
-	for i := 0; i< queue_size-2; i++{
-		if orderQueue[id][i].floor == floor { //&& (orderQueue[i].Btn == btn|| orderQueue[i].Btn == elevio.BT_Cab) {
-			orderQueue[id][i] = orderQueue[id][i+1]
-		}
+	var prev orderStruct
+
+	orderQueue[id][0].Floor = -1
+	orderQueue[id][0].Button = 0
+
+	for i := 1; i < queue_size ; i++ {
+		orderQueue[id][i] = prev
 	}
 
-	orderQueue[id][queue_size-1].floor = -1
-	orderQueue[id][queue_size-1].btn = 0
+	orderQueue[id][queue_size-1].Floor = -1
+	orderQueue[id][queue_size-1].Button = 0
 
 	fmt.Printf("Order removed. \n New order queue: %+v\n", orderQueue)
 }
 
-
+/*
 func CheckStop(floor int, dir elevio.MotorDirection, id int) bool{	
 	var btn elevio.ButtonType
 	if dir == elevio.MD_Up{
@@ -95,5 +102,50 @@ func CheckStop(floor int, dir elevio.MotorDirection, id int) bool{
 	} else if dir == elevio.MD_Down{
 		btn = elevio.BT_HallDown
 	}
-	return orderQueue[id][0].floor == floor && (orderQueue[id][0].btn == btn || orderQueue[id][0].btn == elevio.BT_Cab) 
+	return orderQueue[id][0].floor == floor && (orderQueue[id][0].Button == btn || orderQueue[id][0].Button == elevio.BT_Cab) 
+}
+*/
+
+func Queue(order_chan chan<- OrderStruct) {//In channels: drv_buttons (add order) , floor reached (remove order) , costfunction. Out : push Order
+	InitQueue()
+	var prev_local_order OrderStruct
+
+	drv_buttons := make(chan elevio.ButtonEvent)
+	defer close(drv_buttons)
+	//broadcast_costrequest := make(chan )
+
+	go elevio.PollButtons(drv_buttons)
+
+	//go bcast.OrderAssigning(broadcast_costrequest, order_assigned)
+	//go bcast.OrderReceiver()
+
+	for {
+		select{
+		case button_input := <- drv_buttons:   //Button input from elevator
+			var new_order OrderStruct
+
+			new_order.Button = button_input.Button
+			new_order.Floor = button_input.Floor
+
+			fmt.Printf("Button input: %+v , Floor: %+v\n", new_order.Button, new_order.Floor)
+			
+			//Add to watchdog?
+			//if new_order.button
+			//broadcast_costrequest <- new_order
+			//else cabcall
+			//broadcast_cabcall
+			//addToQueue(new_order, localID)
+
+			//Unnecessary below?
+			//assignedorder <- order_assigned
+			addToQueue(new_order, localID)
+
+        //case orderreceived
+		default:
+			if orderQueue[localID][0] != prev_local_order && orderQueue[localID][0].Floor != -1 {
+				prev_local_order = orderQueue[localID][0]
+				order_chan <- orderQueue[localID][0]
+			}
+		}
+	}
 }
