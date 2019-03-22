2e84531036b398c8392a81ffe2e3a5c832db2565
erlendb <erlend@elendig.no>
2019-03-22 17:24:55 +0100

    Add functionality to find offline local IP

--- networkModule/localip/localip.go
+++ networkModule/localip/localip.go
@@ -14,12 +14,30 @@ func LocalIP() (string, error) {
 	if localIP == "" {
 		conn, err := net.DialTCP("tcp4", nil, &net.TCPAddr{IP: []byte{8, 8, 8, 8}, Port: 53})
 		if err != nil {
-			return "", err
+			return OfflineLocalIP() // return "",err
 		}
 		defer conn.Close()
 		localIP = strings.Split(conn.LocalAddr().String(), ":")[0]
 	}
 	return localIP, nil
+}
+
+
+func OfflineLocalIP() (string, error) {
+	name, err := os.Hostname()
+	if err != nil{
+		return "", err
+	}
+	addrs, err := net.LookupHost(name)
+	if err != nil{
+		return "", err		
+	}
+	var offlineIP string
+	offlineIP = ""
+	for _, a := range addrs {
+		offlineIP += a
+	}
+	return offlineIP, nil
 }
 
 func SetPID() string {
