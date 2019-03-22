f6730e5be1312916821aeea271457feb9166824a
toristvan <tikjetsa@stud.ntnu.no>
2019-03-22 13:29:42 +0100

    place slave and master functionality into one function

--- networkModule/bcast/ord_assign.go
+++ networkModule/bcast/ord_assign.go
@@ -25,72 +25,71 @@ const {
 //need two goroutines or seperate functions 
 //to make sure only channel on button receiving program is altered
 
-func MasterOrder(tra_order_chan <-chan* queue.OrderStruct){
-	var currcost int = 10 //maxORder
-	var first bool = true
+func DistributeOrder(start_order_chan <-chan* queue.OrderStruct, add_order_chan chan*<- queue.OrderStruct){
+	var lowest_cost int = 10 //maxORder
+	var first_cost bool = true
 	var best_elev int =-1
 	var master bool = false
+
+	trans_order := make (chan OrderStruct)
+	rec_order := make (chan OrderStruct)
+	
+	go bcast.Receiver(port, rec_order)
+	go bcast.Transmitter(port, trans_order)
+
 	for{
 		select{
-		case new_order := <-tra_order_chan:
+		case new_order := <-start_order_chan:
 			select{
 			case new_order.Cmd == queue.CostReq:
 				new_order.cdm = queue.CostSend
 				master = true
-				Transmit(new_order)
-			case new_order.Cmd == queue.OrdrAssign:
+				trans_order <- new_order
+			}
+		case new_order := <-rec_order:
+			switch new_order.Cmd{
+			case queue.CostSend:
+				var cost int = queue.CostFunction(new_order)
+				new_order.ElevID = LocalID
+				new_order.Cmd = queue.OrdrAssign
+				trans_order <- new_order //transmit new order
+			case queue.OrdrAssign:
 				if master{
-					if first{
+					if first_cost{
 						ticker := time.Ticker(100*Millisecond)
-						first = false
+						first_cost = false
 					}
-					if new_order.Cost<currcost{
-						currcost = new_order.Cost
+					if new_order.Cost < lowest_cost{
+						lowest_cost = new_order.Cost
 						best_elev = new_order.ElevID
 					}
 					if ticker.c{
 						new_order.ElevID = best_elev
+						new_order.Cost = lowest_cost
 						new_order.Cmd = queue.OrdrAdd
-						new_order.Cost = currcost
-						Transmit(new_order)
+						trans_order <- new_order //transmit new order
 						master = false
-						first = true
-						currcost = 10 //maxcost
+						first_cost = true
+						lowest_cost = 10 //maxcost
+						best_elev = -1
 					}
 				}
+			case queue.OrdrAdd:
+				if new_order.ElevID == LocalID{
+					add_order_chan <- new_order //add order to queue
+					new_order.Cmd = queue.OrdrConf
+					trans_order <- new_order //transmit new order
+				}
+			case queue.OrdrConf:
+				if new_order.ElevID != LocalID{
+					add_order_chan <- new_order
 
+				}
 			}
-
 		}
 	}
 }
 
 
-//receive_order := make(chan queue.OrderStruct)
-//go bcast.Receiver(16569, receive_order)
-//go bcast.ReceiveOrders(receive_order)
-
-func SlaveOrder(rec_order_chan <-chan* queue.OrderStruct, add_order_chan chan*<- queue.OrderStruct){
-	var currcost int = 10 //maxcost
-	for{
-		select{
-		case new_order := <-rec_order_chan:
-			select{
-			case new_order.Cmd == queue.CostSend:
-				var cost int = queue.CostFunction(new_order) //type for cost instead of int
-				new_order.ElevID = LocalID
-				new_order.Cmd = queue.OrdrAssign
-				Transmit(new_order) 
-			case new_order.Cmd == queue.OrdrAdd && new_order.ElevID == LocalID:
-				add_order_chan<- new_order
-				new_order.Cmd = queue.OrdrConf
-				Transmit(new_order)
-			case new_order.Cmd == queue.OrdrConf && new_order.ElevID != LocalID:
-				add_order_chan<-new_order
-			}
-			
-		}
-	}
-}
 
 
