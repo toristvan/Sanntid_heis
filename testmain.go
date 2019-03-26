d16cdd8b04b3aff5bdd10e3d2aed27afc554b1d6
toristvan <tikjetsa@stud.ntnu.no>
2019-03-26 22:04:11 +0100

    Add elevdead and offline check in main

--- testmain.go
+++ testmain.go
@@ -7,68 +7,17 @@ import (
     "./driverModule/elevio"
     "./fsmModule"
     "./networkModule/bcast"
+    "./networkModule/peers"
     "time"
     "os/exec"
     ."fmt"
 )
-//var primary bool = false
 
 type networkOrderStruct struct{
     Order config.OrderStruct
     req_update bool
     send_update bool
 }
-
-//Ser ut til at man får samme problem som sånn med IO.
-//Nå er broacast funksjonaliteten satt en overordnet funksjon
-//Fungerer ganske bra. Eneste ulempen er at kanalene for å sending og mottakning får en sabla lang vei å gå 
-//Som det er implementert nå går kanalbanen: broadCastHub -> Elevrunner -> Queue -> DistributeOrder (noe som er litt jalla)
-/*
-func broadCastHub(recive_chan  chan <- config.OrderStruct, transmit_chan <-chan config.OrderStruct, req_update_queue <- chan bool, Offline_notify_chan chan<- bool){
-    var port int = 20007
-    var txPacket networkOrderStruct
-
-    trans         := make (chan networkOrderStruct)
-    rec           := make (chan networkOrderStruct)
-    offline_alert := make (chan bool)
-
-    go bcast.Receiver(port,rec)
-    go bcast.Transmitter(port, offline_alert, trans)
-
-    for{
-        select{
-            case distribute_rec := <-rec:
-                recive_chan <- distribute_rec.Order
-                if distribute_rec.req_update {
-
-                    var queue = queue.RetriveQueue()
-
-                    txPacket.Order = queue[0][0]
-
-                    Println(txPacket.Order)
-                    txPacket.req_update = false
-                    trans <- txPacket
-                }
-
-            case distribute_trans := <- transmit_chan:
-                Println("Transmitting order")
-                txPacket.Order = distribute_trans
-                trans <- txPacket
-
-            case is_offline := <-offline_alert: 
-                if is_offline {
-                    Offline_notify_chan <- is_offline
-                }
-
-            case tmp := <- req_update_queue:
-                if tmp {
-                    txPacket.req_update = true
-                    trans <- txPacket
-                }
-        }
-    }
-}
-*/
 
 func initElevNode(){
     var num_of_elev int = 3
@@ -89,66 +38,33 @@ func initElevNode(){
 }
 
 
-func backUp(){
-    backUpCmd := exec.Command("gnome-terminal", "-x", "go", "run", "/home/student/Desktop/GR61REAL/project-gruppe-61-real/testmain.go")
-
-    primary := false
-
-    backUp_transmit := make(chan string)
-    backUp_receive := make(chan string)
-    //offline_chan := make(chan bool)
-
-    go bcast.Transmitter((20070 + config.LocalID)/*, offline_chan*/, backUp_transmit)
-    go bcast.Receiver((20070 + config.LocalID), backUp_receive)
-
-    for{
-    	timeoutTicker := time.NewTicker(1000*time.Millisecond)
-        defer timeoutTicker.Stop()
-
-        select{
-        case <- backUp_receive:
-        	Println("Backup confirmation")
-        case <- timeoutTicker.C:
-        	if !primary {
-        		backUp_transmit <- "confirm"
-        		//select {
-        		//case offline_check := <- offline_chan:
-        		//	if !offline_check {
-			            err := backUpCmd.Run()
-
-			            if err != nil {
-			                Println(err)
-			            }
-			            primary = true
-		        //	} else {
-	        	//		Println("Offline, no spawn")
-		        //	}
-		        //}
-	        }
-        }
-    }
-}
-
-
 
 func main() {
     //Queue channels
     add_order_chan := make(chan config.OrderStruct) 
 	distr_order_chan := make(chan config.OrderStruct)  
 	delete_order_chan := make(chan config.OrderStruct)
+    is_dead_chan := make (chan bool)
+
 
 	//elevrunner channels
   	raw_order_chan   := make (chan config.OrderStruct)  
   	execute_chan  := make (chan config.OrderStruct)    
   	elev_cmd_chan := make (chan config.ElevCommand)
+    offline_chan := make (chan bool)
     
     initElevNode()
-    //elevio.Init(Sprintf("localhost:2000%d", config.LocalID)) //, num_floors)  //For simulators
-    elevio.Init(Sprintf("localhost:15657"))//, num_floors)                      //For elevators
+    elevio.Init(Sprintf("localhost:1000%d", config.LocalID)) //, num_floors)  //For simulators
+    //elevio.Init(Sprintf("localhost:15657"))//, num_floors)                      //For elevators
+
+    //elevstatus goroutines
+    go elevclient.IsElevDead(is_dead_chan)
+
+    go peers.CheckOffline(20003, offline_chan)
 
     //Tidligere init i queue/Queue
     go elevclient.ElevRunner(elev_cmd_chan, delete_order_chan )
-    go queue.DistributeOrder(distr_order_chan, add_order_chan, delete_order_chan)
+    go queue.DistributeOrder(distr_order_chan, add_order_chan, delete_order_chan, offline_chan)
     go queue.ReceiveOrder(add_order_chan)
 
     //Tidligere Init i elevatorClient/ElevRunner
@@ -158,9 +74,19 @@ func main() {
     go elevclient.ExecuteOrder(execute_chan)
 
     //Init i main
-    go backUp()
+    //go backUp()
     
     for {
+        select{
+        case deadness := <- is_dead_chan:
+            switch deadness{
+            case true:
+                Printf("DEAD!\n")
+            case false:
+                Printf("ALIVE!\n")
+            }
+
+        }
 		
 		time.Sleep(1*time.Second)
 
