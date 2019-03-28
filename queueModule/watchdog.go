d458105ce126499746bc79307b5fc7e59f712e97
jaf111 <jan_aleksander@hotmail.com>
2019-03-28 22:16:25 +0100

    Probably functional watchdog

--- queueModule/watchdog.go
+++ queueModule/watchdog.go
@@ -24,12 +24,14 @@ func Watchdog(distr_order_chan chan<- config.OrderStruct){
       if i != config.LocalID {
         for j := 0; j< Queue_size; j++{
           if orderQueue[i][j].Floor != -1 && timeout(orderQueue[i][j]){
-            Println("Watchdog caught HIV")
+            Println("Watchdog caught a timeout!")
             order_to_retransmit := orderQueue[i][j]
             switch orderQueue[i][j].Button {
             case config.BT_HallUp, config.BT_HallDown:
               orderQueue[i][j] = invalidateOrder(orderQueue[i][j])
+              Println("'Twas a hall call. I shall do it myself!")
             case config.BT_Cab:
+              Printf("Remember to complete your cabcall in floor %d, Elev %d :)\n", orderQueue[i][j].Floor, i)
             }
             order_to_retransmit.Cmd = config.OrdrRetrans
             distr_order_chan <- order_to_retransmit
